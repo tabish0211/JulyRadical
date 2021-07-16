f5e58864fb09c61ea5e12db5a79a2c6e189ade31
tabish0211 <tabish0211@gmail.com>
2021-07-16 09:48:44 +0530

    till exception

--- JulyRadical/Program.cs
+++ JulyRadical/Program.cs
@@ -11,9 +11,6 @@ namespace JulyRadical
     {
         static void Main(string[] args)
         {
-            calc obj = new calc();
-            obj.Divide();
-
             Console.ReadLine();
 
         }
@@ -21,85 +18,5 @@ namespace JulyRadical
     }
 
 
-    class calc
-    {
-
-        public void Divide()
-        {
-
-            int a, b, r;
-            Console.WriteLine("enter a and b values");
-            a = Convert.ToInt32(Console.ReadLine());
-            b = Convert.ToInt32(Console.ReadLine());
-
-            try
-            {
-               // string v = null;
-                //string z = v.ToString();
-                MyMethod();
-                //r = a / b;
-                //Console.WriteLine(r);
-            }
-
-
-            catch (MyCustomException ex)
-            {
-                Console.WriteLine(ex.Message);
-
-            } 
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine(ex.Message);
-
-            }
-            catch (NullReferenceException ex)
-            {
-                Console.WriteLine(ex.Message);
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("may be you have done some wrong input like zero so we cant process");
-
-            }
-
-            finally
-            { 
-            
-                //always executes--whether exceptions cimes or not it will always executed 
-            
-            
-            }
-
-
-          
-
-            Console.WriteLine("Program is about to exit");
-          
-
-        
-        }
-
-        private void MyMethod()
-        {
-            int i = 1;
-            if (i==1)
-            {
-                throw new MyCustomException();
-            }
-        }
-
-    
-    
-    }
-
-    class MyCustomException : Exception
-    {
-        public MyCustomException():base("This is my custom Message")
-        {
-
-        }
-
-    }
    
 }
